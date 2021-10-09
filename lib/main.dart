ff3adcae589c2aa2bf06a0f6bacd5d1cb5274e6a
mvietnhi <mvietnhi@gmail.com>
2021-10-10 01:36:11 +0700

    feat(login): update
    + test git client

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/home/home_screen.dart';
+import 'package:shopping_app/screens/login.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: LoginScreen(),
     );
   }
 }
