d944fcd95748c7b136934112ba74f8896be8008e
Trinh Thai <ttttrinh132@gmail.com>
2021-10-09 22:25:33 +0700

    no message

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/home/product_item_view.dart';
+import 'package:shopping_app/models/product_model.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<ProductModel> productList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    productList = List.generate(
+        10,
+        (index) => ProductModel(
+            "https://cdn.vuahanghieu.com/unsafe/0x500/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2021/07/giay-the-thao-puma-rs-fast-running-system-white-black-men-casual-shoes-380562-03-mau-trang-den-610200e316e76-29072021081411.jpg",
+            "Product $index",
+            "Brand $index",
+            index * 100));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        drawer: Drawer(
+          child: ListView(
+              shrinkWrap: true,
+              children: List.generate(15, (index) {
+                return index == 15
+                    ? SizedBox(height: 60)
+                    : ListTile(title: Text('Item $index'));
+              })),
+        ),
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: Colors.black),
+          title: Text(
+            'Sneex',
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          // leading: IconButton(
+          //   onPressed: () {
+          //     print('Tapped menu');
+          //   },
+          //   icon: Icon(Icons.menu, color: Colors.black)
+          // ),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  print('Tapped cart');
+                },
+                icon: Icon(
+                  Icons.add_shopping_cart_outlined,
+                  color: Colors.black,
+                )),
+          ],
+        ),
+        body: Container(
+            margin: EdgeInsets.all(5),
+            child: GridView.builder(
+                itemCount: productList.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 2,
+                    crossAxisSpacing: 2,
+                    mainAxisExtent: 275),
+                itemBuilder: (context, index) {
+                  return ProductItemView(productList[index]);
+                })));
+  }
+}
---
+++ lib/home/product_item_view.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/models/product_model.dart';
+
+class ProductItemView extends StatelessWidget {
+  final ProductModel productModel;
+
+  ProductItemView(this.productModel, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        print('Tapped product');
+      },
+      child: Container(
+        margin: EdgeInsets.all(5), // to show shadow
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                blurRadius: 2,
+                spreadRadius: 2,
+              )
+            ]),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.all(5),
+              height: 170,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                      image: NetworkImage(productModel.imageUrl),
+                      fit: BoxFit.cover)),
+            ),
+            SizedBox(height: 4),
+            Text(productModel.name,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+            // SizedBox(height: 4),
+            Text(productModel.brand,
+                style: TextStyle(fontSize: 12, color: Colors.grey)),
+            SizedBox(height: 4),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 5),
+              child: Row(
+                children: [
+                  Text("\$${productModel.price}",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                  Spacer(),
+                  Icon(
+                    Icons.add_shopping_cart,
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 15),
+          ],
+        ),
+      ),
+    );
+  }
+}
